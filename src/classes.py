e22c950b13ca9fdf29d640107d6eea4567941ce7
Roman Kudryashov <romkud55@gmail.com>
2024-04-03 22:18:49 +0300

    REFACTOR: new classes for file formats

--- src/classes.py
+++ src/classes.py
@@ -221,29 +221,6 @@ class JSONVacancyStorage(VacancyStorage):
 
     def _load_data(self) -> list:
         """
-        Метод-коннектор для работы с разными форматами файлов.
-
-        Поддерживаемые форматы: json, csv, txt, xlsx.
-
-        :return: Список вакансий, сохраненных в файлах (в формате словарей).
-        """
-
-        _, extension = os.path.splitext(self.filename)
-
-        match extension:
-            case '.json':
-                return self._load_data_json()
-            case '.csv':
-                return self._load_data_csv()
-            case '.txt':
-                return self._load_data_txt()
-            case '.xlsx':
-                return self._load_data_excel()
-            case _:
-                raise ValueError(f"Неподдерживаемый формат файла: {extension}")
-
-    def _load_data_json(self) -> list:
-        """
         Загружает данные из json файла.
 
         :return: Список вакансий, сохраненных в файлах (в формате словарей).
@@ -252,59 +229,7 @@ class JSONVacancyStorage(VacancyStorage):
         with open(self.filename, 'r', encoding='utf-8') as file:
             return json.load(file)
 
-    def _load_data_csv(self) -> list:
-        """
-        Загружает данные из csv файла.
-
-        :return: Список вакансий, сохраненных в файлах (в формате словарей).
-        """
-
-        with open(self.filename, newline='', encoding='utf-8') as csvfile:
-            return list(csv.DictReader(csvfile))
-
-    def _load_data_txt(self) -> list:
-        """
-        Загружает данные из txt файла.
-
-        :return: Список вакансий, сохраненных в файлах (в формате словарей).
-        """
-
-        with open(self.filename, 'r', encoding='utf-8') as file:
-            return [line.strip() for line in file.readline()]
-
-    def _load_data_excel(self) -> list:
-        """
-        Загружает данные из xlsx файла.
-
-        :return: Список вакансий, сохраненных в файлах (в формате словарей).
-        """
-
-        data_frame = pd.read_excel(self.filename)
-
-        return data_frame.to_dict('records')
-
     def _save_data(self, data: list) -> None:
-        """
-        Метод-коннектор для работы с разными форматами файлов.
-
-        Поддерживаемые форматы: json, csv, txt, xlsx.
-
-        :param data: Список вакансий (в формате словарей) для сохранения.
-        """
-
-        _, extension = os.path.splitext(self.filename)
-
-        match extension:
-            case '.json':
-                self._save_data_json(data)
-            case '.csv':
-                self._save_data_csv(data)
-            case '.txt':
-                self._save_data_txt(data)
-            case '.xlsx':
-                self._save_data_excel(data)
-
-    def _save_data_json(self, data: list) -> None:
         """
         Сохраняет данные в json файл.
 
@@ -314,7 +239,85 @@ class JSONVacancyStorage(VacancyStorage):
         with open(self.filename, 'w', encoding='utf-8') as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
 
-    def _save_data_csv(self, data: list) -> None:
+
+class CSVVacancyStorage(VacancyStorage):
+    """
+    Класс для хранения информации о вакансиях в формате CSV.
+
+    Этот класс поддерживает добавление, поиск и удаление вакансий в хранилище, представленном JSON-файлом.
+
+    Атрибуты:
+        - filename (str): Путь к файлу CSV, используемому для хранения данных о вакансиях.
+
+    Методы:
+        - add_vacancy(vacancy_data): Добавляет новую вакансию в хранилище.
+        - get_vacancies(search_criteria): Возвращает список вакансий, соответствующих заданным критериям поиска.
+        - delete_vacancies(search_criteria): Удаляет вакансии, соответствующие заданным критериям поиска, из хранилища.
+    """
+
+    def __init__(self, filename: str) -> None:
+        """
+        Инициализирует экземпляр класса CSvVacancyStorage.
+
+        :param filename: Путь к CSV-файлу для хранения данных о вакансиях.
+        """
+
+        self.filename = filename
+
+    def add_vacancy(self, vacancy_data: dict) -> None:
+        """
+        Добавляет новую вакансию в хранилище.
+
+        :param vacancy_data: Словарь с данными о вакансии для добавления.
+        """
+
+        try:
+            data = self._load_data()
+        except FileNotFoundError:
+            data = []
+        except json.decoder.JSONDecodeError:
+            data = []
+
+        data.append(vacancy_data)
+        self._save_data(data)
+
+    def get_vacancies(self, search_criteria: dict) -> list:
+        """
+        Возвращает список вакансий, соответствующих заданным критериям поиска.
+
+        :param search_criteria: Словарь с критериями для поиска вакансий.
+        :return: Список вакансий (в формате словарей), соответствующих критериям поиска.
+        """
+
+        data = self._load_data()
+        result = [vacancy for vacancy in data if all(vacancy.get(key) == value for key, value in
+                                                     search_criteria.items())]
+
+        return result
+
+    def delete_vacancies(self, search_criteria: dict) -> None:
+        """
+        Удаляет вакансии, соответствующие заданным критериям поиска, из хранилища.
+
+        :param search_criteria: Словарь с критериями для поиска и удаления вакансий.
+        """
+
+        data = self._load_data()
+        data = [vacancy for vacancy in data if not all(vacancy.get(key) == value for key, value in
+                                                       search_criteria.items())]
+        self._save_data(data)
+
+    def _load_data(self) -> list:
+        """
+        Загружает данные из csv файла.
+
+        :return: Список вакансий, сохраненных в файлах (в формате словарей).
+        """
+
+        with open(self.filename, newline='', encoding='utf-8') as csvfile:
+            return list(csv.DictReader(csvfile))
+
+    def _save_data(self, data: list) -> None:
         """
         Сохраняет данные в csv файл.
 
@@ -328,7 +331,85 @@ class JSONVacancyStorage(VacancyStorage):
             writer.writeheader()
             writer.writerows(data)
 
-    def _save_data_txt(self, data: list) -> None:
+
+class TXTVacancyStorage(VacancyStorage):
+    """
+    Класс для хранения информации о вакансиях в формате TXT.
+
+    Этот класс поддерживает добавление, поиск и удаление вакансий в хранилище, представленном JSON-файлом.
+
+    Атрибуты:
+        - filename (str): Путь к файлу TXT, используемому для хранения данных о вакансиях.
+
+    Методы:
+        - add_vacancy(vacancy_data): Добавляет новую вакансию в хранилище.
+        - get_vacancies(search_criteria): Возвращает список вакансий, соответствующих заданным критериям поиска.
+        - delete_vacancies(search_criteria): Удаляет вакансии, соответствующие заданным критериям поиска, из хранилища.
+    """
+
+    def __init__(self, filename: str) -> None:
+        """
+        Инициализирует экземпляр класса JSONVacancyStorage.
+
+        :param filename: Путь к JSON-файлу для хранения данных о вакансиях.
+        """
+
+        self.filename = filename
+
+    def add_vacancy(self, vacancy_data: dict) -> None:
+        """
+        Добавляет новую вакансию в хранилище.
+
+        :param vacancy_data: Словарь с данными о вакансии для добавления.
+        """
+
+        try:
+            data = self._load_data()
+        except FileNotFoundError:
+            data = []
+        except json.decoder.JSONDecodeError:
+            data = []
+
+        data.append(vacancy_data)
+        self._save_data(data)
+
+    def get_vacancies(self, search_criteria: dict) -> list:
+        """
+        Возвращает список вакансий, соответствующих заданным критериям поиска.
+
+        :param search_criteria: Словарь с критериями для поиска вакансий.
+        :return: Список вакансий (в формате словарей), соответствующих критериям поиска.
+        """
+
+        data = self._load_data()
+        result = [vacancy for vacancy in data if all(vacancy.get(key) == value for key, value in
+                                                     search_criteria.items())]
+
+        return result
+
+    def delete_vacancies(self, search_criteria: dict) -> None:
+        """
+        Удаляет вакансии, соответствующие заданным критериям поиска, из хранилища.
+
+        :param search_criteria: Словарь с критериями для поиска и удаления вакансий.
+        """
+
+        data = self._load_data()
+        data = [vacancy for vacancy in data if not all(vacancy.get(key) == value for key, value in
+                                                       search_criteria.items())]
+        self._save_data(data)
+
+    def _load_data(self) -> list:
+        """
+        Загружает данные из txt файла.
+
+        :return: Список вакансий, сохраненных в файлах (в формате словарей).
+        """
+
+        with open(self.filename, 'r', encoding='utf-8') as file:
+            return [line.strip() for line in file.readline()]
+
+    def _save_data(self, data: list) -> None:
         """
         Сохраняет данные в txt файл.
 
@@ -339,6 +420,91 @@ class JSONVacancyStorage(VacancyStorage):
             for vacancy in data:
                 file.write(str(vacancy) + '\n')
 
-    def _save_data_excel(self, data: list) -> None:
+
+class XLSXVacancyStorage(VacancyStorage):
+    """
+    Класс для хранения информации о вакансиях в формате JSON.
+
+    Этот класс поддерживает добавление, поиск и удаление вакансий в хранилище, представленном JSON-файлом.
+
+    Атрибуты:
+        - filename (str): Путь к файлу JSON, используемому для хранения данных о вакансиях.
+
+    Методы:
+        - add_vacancy(vacancy_data): Добавляет новую вакансию в хранилище.
+        - get_vacancies(search_criteria): Возвращает список вакансий, соответствующих заданным критериям поиска.
+        - delete_vacancies(search_criteria): Удаляет вакансии, соответствующие заданным критериям поиска, из хранилища.
+    """
+
+    def __init__(self, filename: str) -> None:
+        """
+        Инициализирует экземпляр класса JSONVacancyStorage.
+
+        :param filename: Путь к JSON-файлу для хранения данных о вакансиях.
+        """
+
+        self.filename = filename
+
+    def add_vacancy(self, vacancy_data: dict) -> None:
+        """
+        Добавляет новую вакансию в хранилище.
+
+        :param vacancy_data: Словарь с данными о вакансии для добавления.
+        """
+
+        try:
+            data = self._load_data()
+        except FileNotFoundError:
+            data = []
+        except json.decoder.JSONDecodeError:
+            data = []
+
+        data.append(vacancy_data)
+        self._save_data(data)
+
+    def get_vacancies(self, search_criteria: dict) -> list:
+        """
+        Возвращает список вакансий, соответствующих заданным критериям поиска.
+
+        :param search_criteria: Словарь с критериями для поиска вакансий.
+        :return: Список вакансий (в формате словарей), соответствующих критериям поиска.
+        """
+
+        data = self._load_data()
+        result = [vacancy for vacancy in data if all(vacancy.get(key) == value for key, value in
+                                                     search_criteria.items())]
+
+        return result
+
+    def delete_vacancies(self, search_criteria: dict) -> None:
+        """
+        Удаляет вакансии, соответствующие заданным критериям поиска, из хранилища.
+
+        :param search_criteria: Словарь с критериями для поиска и удаления вакансий.
+        """
+
+        data = self._load_data()
+        data = [vacancy for vacancy in data if not all(vacancy.get(key) == value for key, value in
+                                                       search_criteria.items())]
+        self._save_data(data)
+
+    def _load_data(self) -> list:
+        """
+        Загружает данные из xlsx файла.
+
+        :return: Список вакансий, сохраненных в файлах (в формате словарей).
+        """
+
+        data_frame = pd.read_excel(self.filename)
+
+        return data_frame.to_dict('records')
+
+    def _save_data(self, data: list) -> None:
+        """
+        Сохраняет данные в xlsx файл.
+
+        :param data: Список вакансий (в формате словарей) для сохранения.
+        """
+
         data_frame = pd.DataFrame(data)
         data_frame.to_excel(self.filename, index=False)
